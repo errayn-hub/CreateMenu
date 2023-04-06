81f99697567988b3141da8500cc3316502f33c54
ErRayn <yasintopcu3300@gmail.com>
2023-04-06 11:27:50 +0300

    Initial Commit

---
+++ CreateMenu/CreateMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateMenuApp.swift
+//  CreateMenu
+//
+//  Created by ErRayn on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CreateMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
